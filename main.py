4749b8669dcb0016b386ee248f8d4765d94be69d
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2025-10-02 09:07:01 -0600

    sync

--- main.py
+++
@@ -1,6 +0,0 @@
-
-from views.welcome_view import WelcomeView
-
-v = WelcomeView.load_view("Taylor")
-v.present(style='fullscreen')
-
---
+++ main_ios.py
@@ -0,0 +1,29 @@
+# An entrypoint for app development
+import ui
+import dragons_share_app
+import sys
+
+STARTING_PAGE = '/hello'
+
+@ui.in_background
+def launch_background_app():
+  app = dragons_share_app.create_app()
+  
+  with app.app_context():
+    app.run(use_reloader=False, debug=True, port=5001)      
+
+        
+def show_web_browser():
+  wv = ui.WebView()
+  wv.right_button_items = [
+    ui.ButtonItem(image=ui.Image('iob:ios7_arrow_forward_32'), action=lambda t: wv.go_forward()),
+    ui.ButtonItem(image=ui.Image('iob:ios7_refresh_empty_32'), action=lambda t: wv.reload()),
+    ui.ButtonItem(image=ui.Image('iob:chevron_left_24'), action=lambda t: wv.go_back())]
+  wv.present(style='panel')
+  wv.load_html(f'''<a href="http://127.0.0.1:5001{STARTING_PAGE}"><h1>Go to app</h1></a>
+  <script>window.location.href="http://127.0.0.1:5001{STARTING_PAGE}";</script>''')
+
+
+if __name__ == '__main__':
+  launch_background_app()
+  show_web_browser()
